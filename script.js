65a8984ff85103d360344377fbb0addb448cc601
Adline WILSON <adline.wilson@gmail.com>
2021-05-17 15:10:59 +0200

    Ajout d'un local storage

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-$("form").on("submit", function (e) {
-  e.preventDefault();
-  $("main").append(`
-    <div class="card">
-      <div class="card-body">
-      <span style="color: ${$(
-        "input:eq(2)"
-      ).val()}">${$("input:eq(1)").val()}</span> - Le ${$("input:first").val()}
+function refresh() {
+  $("section").html("")
+  var list = JSON.parse(localStorage.getItem("anniversaires"))
+  if (list) {
+    for (var i = 0; i < list.length; i++) {
+      $("section").append(`
+        <div class="card mb-3">
+          <div class="card-body">
+            <span style="color: ${list[i].color}">${list[i].firstname}</span> - Le  ${list[i].date}
+          </div>
+        </div>
+      `)
+    }
+  }
+}
 
-      </div>
-    </div>
-  `);
-});
+refresh()
+
+$("form").on("submit", function (e) {
+  e.preventDefault()
+  var newBirthday = {
+    firstname: $("input:eq(1)").val(),
+    date: $("input:first").val(),
+    color: $("input:last").val(),
+  }
+
+  var list = JSON.parse(localStorage.getItem("anniversaires"))
+  if (!list) {
+    localStorage.setItem("anniversaires", JSON.stringify([newBirthday]))
+    refresh()
+  } else {
+    list.push(newBirthday)
+    localStorage.setItem("anniversaires", JSON.stringify(list))
+    refresh()
+  }
+})
 
 $("button:last").on("click", function () {
-  $("input:first").val("");
-  $("input:last").val("");
-});
-// JE SUIS UN autre COMMENTAIRE
+  $("input:first").val("")
+  $("input:last").val("")
+})

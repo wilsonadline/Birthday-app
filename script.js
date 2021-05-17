f6a26dceeef16c7af30b350d34853d53467bb046
Adline WILSON <adline.wilson@gmail.com>
2021-05-17 12:01:13 +0200

    mise a jour des deux fichiers

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 $("form").on("submit", function (e) {
-  e.preventDefault()
+  e.preventDefault();
   $("main").append(`
     <div class="card">
       <div class="card-body">
-        ${$("input:last").val()} - Le  ${$("input:first").val()}
+      <span style="color: ${$(
+        "input:eq(2)"
+      ).val()}">${$("input:eq(1)").val()}</span> - Le ${$("input:first").val()}
+
       </div>
     </div>
-  `)
-})
+  `);
+});
 
 $("button:last").on("click", function () {
-  $("input:first").val("")
-  $("input:last").val("")
-})
+  $("input:first").val("");
+  $("input:last").val("");
+});
